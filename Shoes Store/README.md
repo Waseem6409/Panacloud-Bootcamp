25165c79125eebab0cf8aaae27df9e856d8c9ba7
Waseem Munir <50287765+Waseem6409@users.noreply.github.com>
2020-07-17 15:03:48 +0500

    Shoe Store

---
+++ Shoes Store/README.md
@@ -0,0 +1,9 @@
+# TodoFirebse
+  
+  [Check out the website](http://shoes-store6409.surge.sh/)
+  
+  To run on your local machine
+  
+  - Clone this repo
+  - `npm install` to install all required dependencies
+  - `npm start` to start on the local server
---
+++ Shoes Store/src/components/Contact.js
@@ -0,0 +1,85 @@
+import React,{useState,useEffect,useRef} from 'react'
+import './css/Contact.css'
+
+function Contact() {
+    const inputRef = useRef();
+    const [isEmailFocused, setIsEmailFocused] = useState(false);
+    const [isPasswordFocused, setIsPasswordFocused] = useState(false);
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    
+  
+    useEffect(() => {
+      setTimeout(() => {
+        inputRef.current.focus();
+      }, 500);
+    }, []);
+  
+    const emailFocusHandler = () => {
+      setIsEmailFocused(true);
+    };
+  
+    const emailBlurHandler = (event) => {
+      if (event.target.value === "") {
+        setIsEmailFocused(false);
+      }
+    };
+  
+    const passwordFocusHandler = () => {
+      setIsPasswordFocused(true);
+    };
+  
+    const passwordBlurHandler = (event) => {
+      if (event.target.value === "") {
+        setIsPasswordFocused(false);
+      }
+    };
+    const emailHandler = (event) => {
+      setEmail(event.target.value);
+    };
+    const passwordHandler = (event) => {
+      setPassword(event.target.value);
+    };
+  
+  
+    return (
+      <div className="login-main-container">
+        <title>Login | Todo</title>
+        <form className="form">
+          <h1 className="title">Contact Us</h1>
+          <div className={`div margin ${isEmailFocused ? "focus" : ""}`}>
+            <h5>Email</h5>
+            <input
+              ref={inputRef}
+              onFocus={emailFocusHandler}
+              onBlur={emailBlurHandler}
+              type="email"
+              value={email}
+              onChange={emailHandler}
+              required
+              autoCorrect="false"
+              spellCheck="false"
+            />
+          </div>
+          <div className={`div margin1 ${isPasswordFocused ? "focus" : ""}`}>
+            <h5>Message</h5>
+            <input
+              onFocus={passwordFocusHandler}
+              onBlur={passwordBlurHandler}
+              value={password}
+              onChange={passwordHandler}
+              required
+              type="textarea"
+            />
+          </div>
+          <input
+            type="submit"
+            className="btn"
+            value="Send Message"
+          />
+        </form>
+      </div>
+    );
+  }
+
+export default Contact
